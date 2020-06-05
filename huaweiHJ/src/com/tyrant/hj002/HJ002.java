3ef42c0a1908495cff8311886bea5086a8869091
tyrant_profiteur <674389994@qq.com>
2020-06-05 20:30:52 +0800

    输入字符串中含有该字符的个数

--- huaweiHJ/src/com/tyrant/hj002/HJ002.java
+++ huaweiHJ/src/com/tyrant/hj002/HJ002.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
  *  输入描述:
  *      第一行输入一个有字母和数字以及空格组成的字符串，第二行输入一个字符。
  *  输出描述:
- *      输出输入字符串中含有该字符的个数。
+ *      输出输入字符串中含有该字符的个数
  **/
 public class HJ002 {
     public static void main(String[] args){
